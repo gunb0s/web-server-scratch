7fe1adfd719ab1afea919f0772ef116724772168
cain <gunbos1031@gmail.com>
2024-03-08 14:32:52 +0900

    ThreadPool에 스레드를 보관하기 위한 벡터 만들기

--- hello/src/lib.rs
+++ hello/src/lib.rs
@@ -1,10 +1,29 @@
-pub struct ThreadPool;
+use std::thread;
+
+pub struct ThreadPool {
+    threads: Vec<thread::JoinHandle<()>>
+};
 
 impl ThreadPool {
+    /// 새 스레드풀 생성
+    ///
+    /// size 는 풀 안의 스레드 개수입니다.
+    ///
+    /// # Panics
+    ///
+    /// `new` 함수는 size 가 0일때 패닉을 일으킵니다
     pub fn new(size: usize) -> ThreadPool {
         assert!(size > 0);
 
-        ThreadPool
+        let mut threads = Vec::with_capacity(size);
+
+        for _ in 0..size {
+
+        }
+
+        ThreadPool {
+            threads
+        }
     }
 
     pub fn execute<F>(&self, f: F)
