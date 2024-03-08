fd6f2283fb71d86646d84581806fa0982baea2b6
cain <gunbos1031@gmail.com>
2024-03-08 14:40:02 +0900

    ThreadPool을 스레드들을 직접 내장하는 대신 Worker 인스턴스들을 내장하게 변경

--- hello/src/lib.rs
+++ hello/src/lib.rs
@@ -1,8 +1,8 @@
 use std::thread;
 
 pub struct ThreadPool {
-    threads: Vec<thread::JoinHandle<()>>
-};
+    workers: Vec<Worker>,
+}
 
 impl ThreadPool {
     /// 새 스레드풀 생성
@@ -15,14 +15,15 @@ impl ThreadPool {
     pub fn new(size: usize) -> ThreadPool {
         assert!(size > 0);
 
-        let mut threads = Vec::with_capacity(size);
+        let mut workers = Vec::with_capacity(size);
 
-        for _ in 0..size {
-
+        for id in 0..size {
+            // 스레드들을 생성하고 벡터 내에 보관합니다
+            workers.push(Worker::new(id));
         }
 
         ThreadPool {
-            threads
+            workers
         }
     }
 
@@ -30,4 +31,20 @@ impl ThreadPool {
         where
             F: FnOnce() + Send + 'static
     {}
+}
+
+struct Worker {
+    id: usize,
+    thread: thread::JoinHandle<()>,
+}
+
+impl Worker {
+    fn new(id: usize) -> Worker {
+        let thread = thread::spawn(|| {});
+
+        Worker {
+            id,
+            thread
+        }
+    }
 }
